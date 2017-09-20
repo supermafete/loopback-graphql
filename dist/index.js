a9826b0c0daa852b7d697507860872e1b09c60cd
Miguel Angel Flores <miguel@alt17.com>
2017-09-20 17:09:00 +0200

    pass context to search

--- dist/index.js
+++ dist/index.js
@@ -391,10 +391,10 @@ function remove(model, args, context) {
 }
 exports.remove = remove;
 function search(model, obj, args, context) {
-    console.log("search", model.modelName, args);
     // was: return getList(model, obj, args, context);
     // to be: return model.search(args.searchTerm);
-    return model.search(args.searchTerm);
+    console.log("CONTEXT", context);
+    return model.search(args.searchTerm, context);
 }
 exports.search = search;
 
--- src/execution.ts
+++ src/execution.ts
@@ -150,7 +150,8 @@ function remove(model, args, context) {
 function search(model: any, obj: any, args: any, context: any) {
   // was: return getList(model, obj, args, context);
   // to be: return model.search(args.searchTerm);
-  return model.search(args.searchTerm);
+  console.log("CONTEXT", context);
+  return model.search(args.searchTerm, context);
 }
 
 export {
